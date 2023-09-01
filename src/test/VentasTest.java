ded8b524db15843804b7d641e6e3cec642f6c806
Fabian Alberto Leon Pertuz <fabian.leon@ludycom.com>
2023-09-01 17:53:34 -0500

    first commit

---
+++ src/test/VentasTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import mx.com.gm.ventas.Orden;
+import mx.com.gm.ventas.Producto;
+
+public class VentasTest {
+    public static void main(String[] args) {
+        Producto producto1 = new Producto("Camisa", 45.000);
+        Producto producto2 = new Producto("Pantalón", 65.000);
+
+        Orden orden1 = new Orden();
+        orden1.agregarProducto(producto1);
+        orden1.agregarProducto(producto2);
+        orden1.mostrarOrden();
+    }
+}
